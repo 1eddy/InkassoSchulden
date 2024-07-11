806e879074d78330a36eb9d0efa4c506b4d06d38
eddy <80455207+1eddy@users.noreply.github.com>
2024-07-11 14:25:05 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,17 @@
 
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = process.env.CosmosDBEndpoint;
+const key = process.env.CosmosDBKey;
+
+const client = new CosmosClient({ endpoint, key });
+const databaseId = "InkassoDatabase";
+const containerId = "SchuldenContainer";
+
+module.exports = async function (context, req) {
+    const { initialDebt, months, monthlyInterest } = req.body;
+
+    if (!initialDebt || !months || !monthlyInterest) {
+        context.res = {
+            status: 400,
+            body: "Please provide all required
